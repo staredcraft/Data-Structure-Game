463fbbad921d0677272c2d68f830edb3c9cf97a9
GenneroDante <gennero.dante@student.oaklandcc.edu>
2014-12-09 14:25:29 -0500

    Update Combat.java
    Updated the combat class and commented.

--- Combat.java
+++ Combat.java
@@ -1,63 +1,169 @@
-
+package finalproj;
 import java.util.*;
 
 public class Combat
 {
-   static Queue<fakecharacter> queue = new Queue();
+   public static Queue<Character> queue = new Queue();
+   protected static Dice die = new Dice();
    public static void main(String[] args)
    {
-      //fakecharacter(String newName, int Hp, int Str, int Dex, int Int, int Weap, int Arm)
-      Dice die = new Dice();
-      fakecharacter One = new fakecharacter("One", 20, 20, 5, 5, 0, 0);
-      fakecharacter Two = new fakecharacter("Two", 20, 5, 19, 5, 0, 0);
-      fakecharacter Three = new fakecharacter("Three", 5, 20, 20, 5, 0 ,0);
-      fakecharacter Four = new fakecharacter("Four", 50, 20, 5, 5, 0 ,0);
+      //Character(String newName, int Hp, int Str, int Dex, int Int, int Weap, int Arm)
+     
+      Character pOne = new Character("One", 20, 20, 20, 5, 0, 0);
+      Character pTwo = new Character("Two", 20, 5, 19, 19, 0, 0);
+      Character pThree = new Character("Three", 5, 20, 18, 5, 0 ,0);
+      Character pFour = new Character("Four", 50, 20, 12, 5, 0 ,0);      
       
-      fakecharacter active;
-      queue.add(Three);
+      Character active;
+      queue.add(pThree);
       System.out.println("Three is first");
-      queue.add(Two);
+      queue.add(pTwo);
       System.out.println("Two is second");
-      queue.add(One);
+      queue.add(pOne);
       System.out.println("One is last");
       
       for (int i = 0; i < 20; i++)
       {
          active = queue.remove();
          System.out.println(active.getName() + "'s turn.");
-         fight(active, Four);
+         magicAttack(active, pFour);
+         active.setSpell(2);
          queue.add(active);
       }
       
    }
    
-   static void fight(fakecharacter agg, fakecharacter def)
+   //This is the actual attack action. Should be called by the Queue after all characters select their actions.
+   static void meleeAttack(Character agg, Character def)
    {
-      if (agg.getActive())
+      activeCheck(agg, def);
+      if ( die.d20() > def.getDex())
       {
-         if (def.getActive())
+         int damage;
+         damage = agg.getStr() / 3 + agg.getWeap() - def.getArm();
+         
+         def.setHp(def.getHp() - damage );
+         if ( def.getHp() < 0)
          {
-            if ( 20 > def.getDex())
+            System.out.println(  agg.getName() + " hit " + def.getName() + " for " + damage + 
+               " damage " + def.getName() + " has died");
+               def.setDeactive();
+         }
+         else
+         {
+            System.out.println( agg.getName() + " hit " + def.getName() + " for " + damage + 
+               " damage " + def.getName() + " has " + def.getHp() + " left.");
+         }
+      }
+      else
+      {
+         System.out.println(agg.getName() + " missed " + def.getName());
+      }
+   }
+   
+   //This is the actual magic action. Should be called by the Queue after all characters select their actions.
+   static void magicAttack(Character agg, Character def)
+   {
+      activeCheck(agg, def);
+      if (agg.hasSpell())
+      {
+         if ( die.d20() > def.getDex())
+         {
+            int damage;
+            int weap = agg.getWeap();
+            
+            if (agg.getWeap() == 5 || agg.getWeap() == 6)
             {
-               int damage;
-               damage = agg.getStr() / 3 + agg.getWeap() - def.getArm();
-               
-               def.setHp(def.getHp() - damage );
-               if ( def.getHp() < 0)
-               {
-                  System.out.println(  agg.getName() + " hit " + def.getName() + " for " + damage + 
-                     " damage " + def.getName() + " has died");
-                     def.setDeactive();
-               }
-               else
-               {
-                  System.out.println( agg.getName() + " hit " + def.getName() + " for " + damage + 
-                     " damage " + def.getName() + " has " + def.getHp() + " left.");
-               }
+               weap = weap - 4;
             }
             else
             {
-               System.out.println(agg.getName() + " missed " + def.getName());
+               weap = 0;
+            }
+            
+            damage =  agg.getInt() / 3 + agg.useSpell() + weap - def.getArm();
+            
+            def.setHp(def.getHp() - damage );
+            if ( def.getHp() < 0)
+            {
+               System.out.println(  agg.getName() + " hit " + def.getName() + " for " + damage + 
+                  " damage " + def.getName() + " has died");
+                  def.setDeactive();
+            }
+            else
+            {
+               System.out.println( agg.getName() + " hit " + def.getName() + " for " + damage + 
+                  " damage " + def.getName() + " has " + def.getHp() + " left.");
+            }
+         }
+         else
+         {
+            System.out.println(agg.getName() + " missed " + def.getName());
+         }
+      }
+      else
+      {
+        System.out.println(agg.getName() + " doesn't have a spell memorized");
+      }
+   }
+   
+   //This is the actual range action. Should be called by the Queue after all characters select their actions.
+   static void rangeAttack(Character agg, Character def)
+   {
+      activeCheck(agg, def);
+      if (agg.getWeap() == 3 || agg.getWeap() == 4)
+      {
+         if ( die.d15() > def.getDex())
+         {
+            int damage;
+            damage = agg.getStr() / 3 + agg.getWeap() - 2/*for weapon damage */ - def.getArm();
+            
+            def.setHp(def.getHp() - damage );
+            if ( def.getHp() < 0)
+            {
+               System.out.println(  agg.getName() + " hit " + def.getName() + " for " + damage + 
+                  " damage " + def.getName() + " has died");
+                  def.setDeactive();
+            }
+            else
+            {
+               System.out.println( agg.getName() + " hit " + def.getName() + " for " + damage + 
+                  " damage " + def.getName() + " has " + def.getHp() + " left.");
+            }
+         }
+         else
+         {
+            System.out.println(agg.getName() + " missed " + def.getName());
+         }
+      }
+      else
+      {
+         System.out.println(agg.getName() + " doesn't have a ranged weapon.");
+      }    
+       
+   }
+   
+   // Sets characters hide.
+   public void Hide(Character hider)
+   {
+      hider.setHide();
+   }
+   
+   // Checks if characters are active or not.
+   static boolean activeCheck(Character agg, Character def)
+   {
+      boolean result = false;
+      if (agg.getActive())
+      {
+         if(def.getActive())
+         {
+            if(def.getHide() == false)
+            {
+               result = true;
+            }
+            else
+            {
+               System.out.println(def.getName() + " is hiding and was not attacked");
             }
          }
          else
@@ -69,5 +175,6 @@ public class Combat
       {
          System.out.println(agg.getName() + " has died and cannot attack");
       }
+      return result;
    }
 }
